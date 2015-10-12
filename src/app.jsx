16a79594050afa0d42ed3579ddc36a70573b26a2
Takayuki WATANABE <takanabe.w@gmail.com>
2015-10-12 14:56:20 +0900

    Add ESLint setting

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import injectTapEventPlugin from "react-tap-event-plugin";
+injectTapEventPlugin();
 import Component from "./component";
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
 
 React.render(
     <Component name="Test-component"/>,
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ module.exports = {
   context: __dirname + "/src",
   entry: {
     jsx: "./app.jsx",
+    css: "./main.css",
     html: "./index.html",
   },
 
@@ -10,13 +11,20 @@ module.exports = {
     filename: "bundle.js",
   },
   module: {
+    preLoaders: [
+        //Eslint loader
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "eslint-loader"},
+    ],
     loaders: [
-      { test: /\.html$/, loader: "file?name=[name].[ext]"},
-      { test: /\.js$/, exclude: /node_modules/, loaders: ["react-hot","babel-loader"]},
-      { test: /\.jsx$/, exclude: /node_modules/, loaders: ["react-hot","babel-loader"]},
+      { test: /\.html$/, loader: "file?name=[name].[ext]" },
+      { test: /\.css$/, loader: "file?name=[name].[ext]" },
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, loaders: ["react-hot","babel-loader"]},
     ],
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-}
+  eslint: {
+    configFile: './.eslintrc'
+  },
+};
